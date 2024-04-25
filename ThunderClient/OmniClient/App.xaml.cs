4fda7096a3583011721a707743f0ee5ac58574d7
Nico <nicolo.zanetti93@gmail.com>
2024-04-25 20:45:51 +0200

    Added initial omniClient implementation

---
+++ ThunderClient/OmniClient/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace OmniClient
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ ThunderClient/OmniClient/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace OmniClient
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
