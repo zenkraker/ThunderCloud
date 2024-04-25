d9d0bdec08ab6e90f07a1d3f214dfe855d9ccedd
Nico <nicolo.zanetti93@gmail.com>
2024-04-25 23:43:47 +0200

    Added recycleBinItem to specify files in the bin

---
+++ ThunderServer/ThunderServer.API/Domain/RecycleBinItem.cs
@@ -0,0 +1,13 @@
+﻿namespace ThunderServer.API.Domain
+{
+    public class RecycleBinItem
+    {
+        public Guid Id { get; set; }
+        public ItemType ItemType { get; set; } // "File" or "Folder"
+        public Guid ItemId { get; set; } // ID of the deleted item
+        public DateTime DeletedAt { get; set; }
+
+        // Navigation properties
+        public virtual ThunderFolder ParentFolder { get; set; }
+    }
+}
