89c45c9127aeae1d935a24aec8d34212f90b252e
Nico <nicolo.zanetti93@gmail.com>
2024-04-25 20:47:10 +0200

    Added first endpoint folder for uploading files

---
+++ ThunderServer/ThunderServer.API/Endpoints/Files/UploadFile.Request.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ThunderServer.API.Endpoints.Files
+{
+    public class FileUploadRequest
+    {
+        [FromForm]
+        public required List<IFormFile> Files { get; init; }
+
+        [FromQuery]
+        public string? PathToStoreFiles { get; init; }
+    }
+}
---
+++ ThunderServer/ThunderServer.API/Endpoints/Files/UploadFile.cs
@@ -0,0 +1,16 @@
+﻿using Ardalis.ApiEndpoints;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ThunderServer.API.Endpoints.Files
+{
+    public class UploadFile : EndpointBaseAsync.WithRequest<FileUploadRequest>.WithActionResult
+    {
+        [HttpPost("/api/files/Upload")]
+        public override async Task<ActionResult> HandleAsync(FileUploadRequest request, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+
+            return Ok();
+        }
+    }
+}
