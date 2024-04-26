fd5a42fe1852290c4f2fb4dfcd54bf2144b38bc6
Nico <nicolo.zanetti93@gmail.com>
2024-04-26 22:23:02 +0200

    Injected new services

--- ThunderServer/ThunderServer.API/Program.cs
+++ ThunderServer/ThunderServer.API/Program.cs
@@ -1,5 +1,9 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
+using System.Reflection;
+using ThunderServer.API.Configurations;
+using ThunderServer.API.Services;
+using ThunderServer.API.Services.Interfaces;
 using ThunderServer.Infratructure.Storage;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,12 +19,20 @@ builder.Services.AddSwaggerGen(c =>
     c.EnableAnnotations();
 });
 
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
 builder.Services.AddLogging();
+
+builder.Services.AddOptions<StorageVolumesConfiguration>().Bind(builder.Configuration.GetSection("Storage-Volumes")).ValidateOnStart();
 
 builder.Services.AddDbContext<ThunderServerContext>(options =>
 {
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+builder.Services.AddTransient<IFileManager, FileManager>();
+
+builder.Services.AddHostedService<FileStorer>();
 
 var app = builder.Build();
 
